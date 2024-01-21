2b9b6bcfb60931bf5f4d3c47771f481922def952
K Hemant Kumar <117855361+hemant-apk@users.noreply.github.com>
2024-01-21 23:15:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,89 +24,75 @@ repo = g.get_repo(repo_name)
 all_commits = repo.get_commits()
 
 # Fetching contributors and their contributes
-contributors_data = {}
-for contributor in repo.get_contributors():
-    commits = repo.get_commits(author=contributor.login)
+def getContributionByUser():
+    contributors_data = {}
+    for contributor in repo.get_contributors():
+        commits = repo.get_commits(author=contributor.login)
+        
+        line_changes = sum(commit.stats.total for commit in commits)
+        file_changes = len({file.filename for commit in commits for file in commit.files})
+
+        merged_pr_counts = getMergedPRCountForUser(contributor.login)
+
+        contributors_data[contributor.login] = {
+            "line_contributed": line_changes,
+            "file_contributed": file_changes,
+            "pr_merged": merged_pr_counts
+        }
     
-    line_changes = sum(commit.stats.total for commit in commits)
-    file_changes = len({file.filename for commit in commits for file in commit.files})
-
-    contributors_data[contributor.login] = {
-        "line_changes": line_changes,
-        "file_changes": file_changes
-    }
-
-
-
+    return contributors_data
       
 # Fetching merged pull requests and count 
-merged_pr_counts = {}
-for pull in repo.get_pulls(state="all"):  
-    if pull.merged:
-        merged_by = pull.merged_by.login
-        merged_pr_counts.setdefault(merged_by, 0)
-        merged_pr_counts[merged_by] += 1
+def getMergedPRCountForUser(username):
+    merged_pr_count = 0
 
+    for pull in repo.get_pulls(state="all"):  
+        if pull.merged and pull.merged_by is not None and pull.merged_by.login == username:
+            merged_pr_count += 1
 
-
-
+    return merged_pr_count
 
 # Fetching no. of pull requests 
-pr_counts = {}
-for pull in repo.get_pulls(state="all"):  
-    user = pull.user.login
-    pr_counts.setdefault(user, 0)
-    pr_counts[user] += 1
+def getPRCreatedByUser():
+    pr_counts = {}
+    for pull in repo.get_pulls(state="all"):  
+        user = pull.user.login
+        pr_counts.setdefault(user, 0)
+        pr_counts[user] += 1
 
-
-
+    return pr_counts
 
 # Fetching pull reviews and count reviews by user
-pr_reviews = {}
-for pull in repo.get_pulls(state="all"): 
-    for review in pull.get_reviews():
-        reviewer = review.user.login
-        pr_reviews.setdefault(reviewer, 0)
-        pr_reviews[reviewer] += 1
-
-
-
-
+def getPRReviewsByUser():
+    pr_reviews = {}
+    for pull in repo.get_pulls(state="all"): 
+        for review in pull.get_reviews():
+            reviewer = review.user.login
+            pr_reviews.setdefault(reviewer, 0)
+            pr_reviews[reviewer] += 1
+    
+    return pr_reviews
 
 # display line change and file change
-@app.route("/user")
+@app.route("/getContributionByUser")
 def user():
+    contributors_data = getContributionByUser()
     user_data = [{"author": author, "count": count} for author, count in contributors_data.items()]
     return jsonify(user_data)
 
-
-# display merge count
-@app.route("/merge_count")
-def get_merge_count():
-    merge_count_data = [{"author": author, "count": count} for author, count in merged_pr_counts.items()]
-    return jsonify(merge_count_data)
-
-
-
-
 #display pull requests
-@app.route("/pull_request")
+@app.route("/getPRCreatedByUser")
 def get_pull_request():
+    pr_counts = getPRCreatedByUser()
     pull_request_data = [{"author": author, "count": count} for author, count in pr_counts.items()]
     return jsonify(pull_request_data)
 
-
-
-
 #display pull reviews
-@app.route("/pr_reviewed")
+@app.route("/getPRReviewedByUser")
 def get_prs_reviewed():
+    pr_reviews = getPRReviewsByUser()
     pr_review_data = [{"author": author, "count": count} for author, count in pr_reviews.items()]
     return jsonify(pr_review_data)
-
-
-
-
 
 g.close()
 
